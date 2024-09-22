1130af522860f236ec529ffd9b912760f97b4e4e
Patrul <aliwka9532@gmail.com>
2024-09-22 17:36:44 +0400

    first commit

---
+++ dom.js
@@ -0,0 +1,375 @@
+let goods = [
+    {
+        product_name: "Kişi köynəyi",
+        product_description: "Mavi rəngdə, 100% pamuk",
+        product_price: 50,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Qadın bluzası",
+        product_description: "Dəri detallı, qara rəngdə",
+        product_price: 60,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Kişi pantolonu",
+        product_description: "Qəhvəyi rəng, kənar cebi",
+        product_price: 75,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Qadın eteyi",
+        product_description: "Qırmızı rəng, mini",
+        product_price: 40,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Kişi dəsmalı",
+        product_description: "Nəqşdar dizayn",
+        product_price: 20,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Qadın çantası",
+        product_description: "Əlgötürən, dəri",
+        product_price: 90,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Kişi botları",
+        product_description: "Qış üçün, suya davamlı",
+        product_price: 120,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Qadın ayaqqabıları",
+        product_description: "Yüksək tapan, lacivərt",
+        product_price: 85,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Kişi papağı",
+        product_description: "Qara rəngdə, dəri",
+        product_price: 45,
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+    },
+    {
+        product_name: "Qadın şalvarı",
+        product_description: "Göy rəngdə, kaşmir",
+        product_price: 70,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Kişi palto",
+        product_description: "Süət dəri, qara rəngdə",
+        product_price: 250,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Qadın jaketi",
+        product_description: "Uzun, qaşqır",
+        product_price: 200,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Kişi kəməri",
+        product_description: "Dəri, metal tokalı",
+        product_price: 40,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Qadın badlonu",
+        product_description: "Retro stil, qızıl rəngdə",
+        product_price: 180,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Kişi şortu",
+        product_description: "Spor stil, elastik",
+        product_price: 60,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Qadın maykası",
+        product_description: "Boyalı nəqş, pamuklu",
+        product_price: 45,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Kişi saatı",
+        product_description: "Qara rəngdə, analog",
+        product_price: 150,
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+    },
+    {
+        product_name: "Qadın bəzək",
+        product_description: "Qızıl, komplekt",
+        product_price: 220,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi atkısı",
+        product_description: "Xəzəl rəng, uzun",
+        product_price: 35,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın bantı",
+        product_description: "Metal detallı, elastik",
+        product_price: 50,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi kostyumu",
+        product_description: "İki parçalı, qara rəngdə",
+        product_price: 320,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın kombinezonu",
+        product_description: "Yaz üçün, açıq rəng",
+        product_price: 150,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi sviteri",
+        product_description: "Düz rəng, yüngül",
+        product_price: 70,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın ziyafət geyimi",
+        product_description: "Düzənşən, yaz üçün",
+        product_price: 95,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi sport formaları",
+        product_description: "Spandex, idman üçün",
+        product_price: 55,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın bikini",
+        product_description: "Tropik nəqş, elastik",
+        product_price: 45,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi çantası",
+        product_description: "Dizayner, əsas bölməsi",
+        product_price: 100,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın ətri",
+        product_description: "Gül ətri, uzunömürlü",
+        product_price: 80,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Kişi şalı",
+        product_description: "Səliqəli, çoxrəngli",
+        product_price: 25,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+    {
+        product_name: "Qadın sutyeni",
+        product_description: "Destəklənmiş, rahat",
+        product_price: 65,
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+    },
+];
+
+let list = document.querySelector('ul')
+let modal = document.getElementById('product-modal');
+let modalContent = document.getElementById('modal-content');
+let closeModalBtn = document.querySelector('.modal-close');
+
+const showList = (arr) => {
+    arr.forEach((item) => {
+        let li = document.createElement('li')
+        li.innerHTML = `
+         <p>${item.product_name}</p>
+         <p>${item.product_description}</p>
+         <p>${item.product_price}</p>
+         <p>${item.store_name}</p>
+         <p>${item.store_address}</p>
+         <button id="btn-info">Info</button>
+         <button id="btn-del">Delete</button>
+        `
+
+        li.querySelector('#btn-info').addEventListener("click", () => {
+            modalContent.innerHTML = `
+                <h3>${item.product_name}</h3>
+                <p>Описание: ${item.product_description}</p>
+                <p>Цена: ${item.product_price}</p>
+                <p>Магазин: ${item.store_name}</p>
+                <p>Адрес: ${item.store_address}</p>
+            `;
+
+
+            modal.style.display = 'block';
+
+        });
+
+        li.querySelector('#btn-del').addEventListener("click", (event) => {
+            goods.splice(goods.indexOf(item), 1)
+            // let allLiS = document.querySelectorAll('li');
+            // console.log(allLiS)
+
+            // allLiS.forEach((item1) => {
+            //     if ( item.product_name === item1.children[0].innerText ) {
+            //         item1.remove()
+            //     }
+            // })
+
+            event.target.parentElement.remove()   // альтернативная замена удалению, самая быстрая
+        })
+
+        list.append(li)
+    })
+
+    closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+}
+
+showList(goods)
+
+
+
+function newElementForHtml(item) {
+    let li = document.createElement('li');
+    li.innerHTML = `
+        <p>${item.product_name}</p>
+        <p>${item.product_description}</p>
+        <p>${item.product_price}</p>
+        <p>${item.store_name}</p>
+        <p>${item.store_address}</p>
+        <button id="btn-info">Info</button>
+        <button id="btn-del">Delete</button>
+    `;
+
+    li.querySelector('#btn-info').addEventListener("click", () => {
+        modalContent.innerHTML = `
+            <h3>${item.product_name}</h3>
+            <p>Описание: ${item.product_description}</p>
+            <p>Цена: ${item.product_price}</p>
+            <p>Магазин: ${item.store_name}</p>
+            <p>Адрес: ${item.store_address}</p>
+        `;
+
+        modal.style.display = 'block';
+    });
+
+    li.querySelector('#btn-del').addEventListener("click", (event) => {
+        event.target.parentElement.remove(); // Удаляем элемент из DOM
+    });
+
+    list.appendChild(li);
+}
+newElementForHtml({
+    product_name: "Qadın 2",
+    product_description: "Destəklənmiş,2",
+    product_price: 565,
+    store_name: "Əliyev 2",
+    store_address: "Bakı 2"
+})
+
+
+
+function sorting(item) {
+    let par3 = document.querySelectorAll('li');
+    let newPar3 = Array.from(par3);
+
+    newPar3.sort((a, b) => {
+
+        let first = parseInt(a.children[2].innerText)
+        let second = parseInt(b.children[2].innerText)
+
+        if (item === "decrease") {
+            return second - first
+        }
+        else {
+            return first - second
+        }
+
+    })
+    list.innerHTML = '';
+    list.append(...newPar3)
+
+}
+sorting("decrease")
+
+
+
+// const searchGods = () => {
+//     let input = prompt()
+//     let listItems = document.querySelectorAll('ul li')
+//     let transformedArray = Array.from(listItems)
+//     let filteredItems = transformedArray.filter((item) => item.children[0].innerText.startsWith(input))
+//     document.querySelector('ul').innerHTML = ''
+//     document.querySelector('ul').append(...filteredItems)
+// }
+
+// searchGods()
+
+
+// const deleteByDOM = () => {
+
+//     let allLiS = document.querySelectorAll('li');
+
+//     allLiS.forEach((item) => {
+//         if (item.children[0].innerText === "Kişi köynəyi") {
+//             item.remove()
+//         }
+//     })
+
+// }
+
+// deleteByDOM()
+
+
+
+// const deleteByArr = () => {
+//     let elementIndex = goods.findIndex((item) => item.product_name === "Kişi kostyumu")
+//     let newGoods = goods.toSpliced(elementIndex, 1)
+//     list.innerHTML = ''
+//     showList(newGoods)
+
+// }
+
+// deleteByArr()
+
